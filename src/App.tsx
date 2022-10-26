a3bd2b03b8e2b5b22ad3560eba5575743ee15c6c
chiyukikana <chiyukikana@icloud.com>
2022-10-26 19:57:24 +0800

    add async/await

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,31 @@ interface Props {}
 const App: React.FC<Props> = () => {
   const [count, setCount] = useState<number>(0);
   const [robotGallery, setRobotGallery] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     document.title = `点击了${count}次`;
   }, [count]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then(setRobotGallery);
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setRobotGallery(data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      }
+      setLoading(false);
+    };
 
-    return () => {};
+    fetchData();
   }, []);
 
   return (
@@ -37,11 +51,16 @@ const App: React.FC<Props> = () => {
       </button>
       <span>count: {count}</span>
       <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotGallery.map((r) => (
-          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
-        ))}
-      </div>
+      {(!error || error !== "") && <div>网站出错：{error}</div>}
+      {!loading ? (
+        <div className={styles.robotList}>
+          {robotGallery.map((r) => (
+            <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+          ))}
+        </div>
+      ) : (
+        <h2>Loading</h2>
+      )}
     </div>
   );
 };
